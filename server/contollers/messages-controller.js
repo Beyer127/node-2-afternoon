6643180a11c03750ce90dc0aa33a208b3185d92c
Beyer127 <67298642+Beyer127@users.noreply.github.com>
2020-07-29 09:40:17 -0700

    node-2-afternoon

--- server/contollers/messages-controller.js
+++ server/contollers/messages-controller.js
@@ -12,9 +12,22 @@ module.exports = {
         res.status(200).send(messages)
     },
     update: (req,res) => {
+        const {text} = req.body
+        const updateId = req.params.id
+        const messageIndex = messages.findIndex(message => message.id == updateId)
+        let message = messages[messageIndex]
 
+        messages[messageIndex] = {
+            id: message.id,
+            text: text || message.text,
+            time: message.time
+        }
+        res.status(200).send(messages)
     },
     delete: (req,res) => {
-
+        const deleteID = req.params.id
+        const messageIndex = message.findIndex(message => message.id == deleteID)
+        messages.splice(messageIndex,1)
+        res.status(200).send(messages)
     }
 }
--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
-const ctrl = require('./controller')
+const ctrl = require('./contollers/messages-controller')
 const app = express()
 const port = 3000
 
 app.use(express.json())
+
+app.get('/api/messages', ctrl.create)
+app.get('/api/messages/:id', ctrl.)
 
 
 app.listen(port, () => console.log(`listening on port ${port}`))
